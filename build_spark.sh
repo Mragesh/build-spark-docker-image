ef860627685c63735b85dc1d885a47e9c2d23176
Mragesh Khandelwal <mragesh.khandelwal@gmail.com>
2020-06-23 13:23:34 +0000

    Do not download spark if it already exists

--- build_spark.sh
+++ build_spark.sh
@@ -2,18 +2,22 @@
 
 export SPARK_VERSION=3.0.0
 export HADOOP_VERSION=3.2
-export DOCKER_IMAGE_TAG=spark3.0.0
+export DOCKER_IMAGE_TAG=latest
 export DOCKER_REPO=mragesh
 
+if [ ! -d "spark" ]
+then
 # download and extract spark
 curl http://mirror.metrocast.net/apache/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz | tar xvz -C . 
-
 # rename the directory
 mv spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION spark
+fi
 
+cd dependencies/spark
 # copy custom dependencies and changes for pyspark
-cp -r --parents custom/spark/* spark/
+cp -r --parents * ../../spark/
 
+cd ../..
 # build docker image
 ./spark/bin/docker-image-tool.sh -p spark/kubernetes/dockerfiles/spark/bindings/python/Dockerfile -r $DOCKER_REPO -t $DOCKER_IMAGE_TAG build
 
